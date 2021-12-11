ef8e9e2b0574ff5f040cceee8d43cffb0a6e6dfd
Cathy Mariza <nmalizac@gmail.com>
2021-12-11 01:28:28 -0600

    changes to index, css

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,19 +1,54 @@
 $(document).ready(function() {
-  $('#clicker').click(function() {
-    $.ajax({
-      dataType: "json",
-      url: "https://random.dog/woof.json",
-      success: function(results) {
-        console.log(results["url"]);
-        if (results["url"].endsWith(".mp4")) {
-          $('#dog').attr("src", "images/blank.png");
-        } else {
-          $('#dog').attr("src", results["url"]);
-        }
-      },
-      error: function(xhr,status,error) {
-        console.log(error);
-      }
-    });
+  jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
+const getJoke = () =>{
+  $.ajax({
+    dataType: "json",
+    method: 'GET',
+    url: "https://official-joke-api.appspot.com/random_joke",
+    success: function(results) {
+      $('#setup').text(results["setup"]);
+      $('#punchline').text(results["punchline"]);
+    },
+    error: function(xhr,status,error) {
+      console.log(error);
+    }
   });
+}
+
+const getPicture = () =>{
+  $.ajax({
+    dataType: "json",
+    method: 'GET',
+    Header: {"X-Requested-With":"XMLHttpRequest"},
+    url: "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true",
+    success: function(results) {
+            $('#size_controller').attr("src",results[0]);
+
+      $('#picture').css("background-image", "url("+results[0]+")");
+
+    },
+    error: function(xhr,status,error) {
+      console.log(error);
+    }
+  });
+}
+  getJoke();
+  getPicture();
+$('#gen_joke').click(function () {
+  getJoke();
+});
+
+$('#gen_image').click(function () {
+  getPicture();
+});
+
+$('#generate').click(function () {
+  getJoke();
+  getPicture();
+});
 });
